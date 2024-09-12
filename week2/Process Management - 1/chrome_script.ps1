9ba34b43995af4e950cb0407962e2705d7b23fd2
devinziegler <devin.ziegler@mymail.champlain.edu>
2024-09-11 23:01:19 -0400

    Added process management scripts

---
+++ week2/Process Management - 1/chrome_script.ps1
@@ -0,0 +1,6 @@
+﻿if (Get-Process -Name chrome -ErrorAction ignore) {
+    Stop-Process -Name chrome
+}
+else {
+    Start-Process -Filepath "C:\Program Files\Google\Chrome\Application\Chrome" -ArgumentList "Champlain.edu"
+}
