557410312118a39e290aa182c9b1f5dc3f0eeb76
devinziegler <devin.ziegler@mymail.champlain.edu>
2024-09-18 23:51:52 -0400

    startstop script

--- Week 3/functions_event_logs.ps1
+++ Week 3/functions_event_logs.ps1
@@ -1,6 +1,7 @@
 ﻿# Get-EventLog System -source Microsoft-Windows-winlogon
+Function LoginEvent($days) {
 
-$loginouts = Get-EventLog System -source Microsoft-Windows-winlogon -After (Get-Date).AddDays(-14)
+$loginouts = Get-EventLog System -source Microsoft-Windows-winlogon -After (Get-Date).AddDays(-$days)
 
 $loginoutsTable = @()
 for ($i=0; $i -lt $loginouts.Count; $i++){
@@ -9,15 +10,41 @@ $event = ""
 if($loginouts[$i].EventId -eq 7001) {$event="Logon"}
 if($loginouts[$i].EventId -eq 7002) {$event="Logoff"}
 
-$user = $loginouts[$i].ReplacementStrings[1]
+$id = $loginouts[$i].ReplacementStrings[1]
+$SID = New-Object System.Security.Principal.SecurityIdentifier($id)
+$user = $SID.Translate([System.Security.Principal.NTAccount])
 
 $loginoutsTable += [pscustomobject]@{"Time" = $loginouts[$i].TimeGenerated; `
                                        "Id" = $loginouts[$i].EventID; `
                                     "Event" = $event;
-                                     "User" = $user;                     
+                                     "User" = $user.value;                     
                                      }
 
 }
+return $loginoutsTable
+}
 
-$loginoutsTable
+Function StartShutTime{
+$startstop = Get-EventLog System -After (Get-Date).AddDays(-30)
 
+$StartStopTable = @()
+for ($i=0; $i -lt $startstop.Count; $i++){
+
+$event = ""
+if($startstop[$i].EventId -eq 6005) {$event="Startup"}
+if($startstop[$i].EventId -eq 6006) {$event="Shutdown"}
+
+$StartStopTable += [pscustomobject]@{"Time" = $startstop[$i].TimeGenerated; `
+                                       "Id" = $loginouts[$i].EventID; `
+                                    "Event" = $event;
+                                     "User" = "System"                   
+                                     }
+
+return $StartStopTable
+}
+}
+
+
+
+#LoginEvent(30)
+StartShutTime
---
+++ Week 3/startstop.ps1
@@ -0,0 +1,20 @@
+﻿Function StartShutTime{
+$startstop = Get-EventLog System -After (Get-Date).AddDays(-30)
+
+$StartStopTable = @()
+for ($i=0; $i -lt $startstop.Count; $i++){
+
+$event = ""
+if($startstop[$i].EventId -eq 6005) {$event="Startup"}
+if($startstop[$i].EventId -eq 6006) {$event="Shutdown"}
+
+$StartStopTable += [pscustomobject]@{"Time" = $startstop[$i].TimeGenerated; `
+                                       "Id" = $startstop[$i].EventID; `
+                                    "Event" = $event;
+                                     "User" = "System"                   
+                                     }
+
+return $StartStopTable
+}
+}
+StartShutTime
