8eb3d42e5220fa45592dcd09a7ae05eb0b8309f5
devinziegler <devin.ziegler@mymail.champlain.edu>
2024-10-03 14:28:11 -0400

    Added Windows Apache Logs

---
+++ Week 3/startstop.ps1
@@ -0,0 +1,21 @@
+﻿Function StartShutTime ($eventID, $days){
+
+$startstop = Get-EventLog System -After (Get-Date).AddDays(-$days) | Where-Object { $_.EventID -in $eventID}
+
+$StartStopTable = @()
+for ($i=0; $i -lt $startstop.Count; $i++){
+
+$event = ""
+if($startstop[$i].EventId -eq 6005) {$event="Startup"}
+if($startstop[$i].EventId -eq 6006) {$event="Shutdown"}
+
+$StartStopTable += [pscustomobject]@{"Time" = $startstop[$i].TimeGenerated; `
+                                       "Id" = $startstop[$i].EventID; `
+                                    "Event" = $event;
+                                     "User" = "System"                   
+                                     }
+
+}
+return $StartStopTable
+}
+StartShutTime -eventID 6005 -days 30
---
+++ Week 4/Windows Apache Logs.ps1
@@ -0,0 +1,32 @@
+﻿#Get-Content C:\xampp\apache\logs\access.log
+
+#Get-Content C:\xampp\apache\logs\access.log -tail 5
+
+#Get-Content C:\xampp\apache\logs\access.log | Select-String -Pattern ' 404 ',' 400 '
+
+#Get-Content C:\xampp\apache\logs\access.log | Where-Object { $_ -notmatch ' 200 '}
+
+#$A = Get-Childitem C:\xampp\apache\logs\*.log | Select-String 'error'
+#$A[-5..-1]
+
+# Get only logs that contain 404, save into $notfounds
+$notfounds = Get-Content C:\xampp\apache\logs\access.log | Select-String ' 404 '
+
+#Define a regex for IP addresses
+$regex = [regex] "\b(?:\d{1,3}\.){3}\d{1,3}\b"
+
+#Get $notfounds records that match to the regex
+$ipsUnorganized = $regex.Matches($notfounds)
+
+#Get ips as pscustomobject
+$ips = @()
+for($i=0; $i -lt $ipsUnorganized.Count; $i++){
+    $ips += [pscustomobject]@{ "IP" = $ipsUnorganized[$i].Value; }
+    
+}
+#$ips | Where-Object { $_.IP -ilike "10.*"}
+
+# Count ips from number 8
+$ipsoftens = $ips | Where-Object { $_.IP -ilike "10.*"}
+$counts = $ipsoftens | Group IP
+$counts | Select-Object Count, Name
