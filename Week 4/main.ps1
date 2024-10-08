a872ab49c0d1be28dd302aecee31d818d8a89ffc
devinziegler <devin.ziegler@mymail.champlain.edu>
2024-10-08 17:38:15 -0400

    added parsing apache logs script

--- Week 4/main.ps1
+++ Week 4/main.ps1
@@ -1,5 +1,6 @@
 ﻿. (Join-Path $PSScriptRoot Apache-logs.ps1)
-
+. (Join-Path $PSScriptRoot parsing_apache_logs.ps1)
 clear
 
-findIp -pageVisited 'index.html' -httpCode '200' -browserName 'chrome' 
+findIp -pageVisited 'index.html' -httpCode '200' -browserName 'chrome'
+$tableRecords | Format-Table -AutoSize -Wrap 
---
+++ Week 4/parsing_apache_logs.ps1
@@ -0,0 +1,19 @@
+﻿function ApacheLogs1(){
+    $logsNotFormatted = Get-Content C:\xampp\apache\logs\access.log 
+    $tableRecords = @()
+
+    for($i=0; $i -lt $logsNotFormatted.Count; $i++) {
+        $Words = $logsNotFormatted[$i].split(" ");
+
+        $tableRecords += [PSCustomObject]@{ "IP" = $words[0]; `
+                                            "Time" = $words[3].Trim('['); `
+                                            "Method" = $words[5].Trim('"'); `
+                                            "Page" = $words[6]; `
+                                            "Protocol" = $words[7]; `
+                                            "Response" = $words[8]; `
+                                            "Referrer" = $words[10]; `
+                                            "Client" = $words[11];}
+    }
+    return $tableRecords | Where-Object { $_.IP -like "10.*" }
+}
+$tableRecords = Apachelogs1
