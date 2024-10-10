ddc1b39d82c7da040f6ba3c2b6dd40353105848a
devinziegler <devin.ziegler@mymail.champlain.edu>
2024-10-10 13:28:58 -0400

    added scripts

---
+++ Week 5/main.ps1
@@ -0,0 +1,4 @@
+﻿. (Join-Path $PSScriptRoot scraping_champlain_classes.ps1)
+
+# gatherClasses
+gatherClasses
---
+++ Week 5/scraping_champlain_classes.ps1
@@ -0,0 +1,58 @@
+﻿function gatherClasses() {
+    $page = Invoke-WebRequest -TimeoutSec 2 http://10.0.17.10/courses.html
+
+    # Get all the tr elements of HTML document 
+    $trs = $page.ParsedHtml.body.GetElementsByTagName("tr")
+
+    # Empty array to hold results
+    $FullTable = @()
+    for($i=1; $i -lt $trs.length; $i++) { 
+
+        # Get every td element of current tr element
+        $tds = $trs[$i].getElementsByTagNAme("td")
+
+        # Want to separate start time and end time from one time field 
+        $Times = $tds[5].innerText.Split("-")
+ 
+        $FullTable += [PSCustomObject]@{
+            "Class Code" = $tds[0].innerText; `
+            "Title"      = $tds[1].innerText; `
+            "Days"       = $tds[4].innerText; `
+            "Time Start" = $Times[0]; `
+            "Time End"   = $Times[1]; `
+            "Instructor" = $tds[6].innerText; `
+            "Location"   = $tds[9].innerText; `
+           
+        }
+   
+    }
+
+    return daysTranslator
+}
+
+function daysTranslator($FullTable) {
+        
+        # Go over every record in the table
+        for($i=0; $i -lt $FullTable.length; $i++) {
+            
+            # Empty array to hold days for every record
+            $Days = @()
+
+            # If you see "M" -> Monday
+            if($FullTable[$i].Days -ilike "m") { $Days += "Monday"}
+            
+            if($FullTable[$i].Days -ilike "*T[TWF]*") { $Days += "Tuesday"}
+           
+            if($FullTable[$i].Days -ilike "w") { $Days += "Wednesday"}
+
+            if($FullTable[$i].Days -ilike "TH") { $Days += "Thursday"}
+
+            if($FullTable[$i].Days -ilike "F") { $Days += "Friday"}
+
+            # Make the Switch
+            $Fulltable[$i].Days = $Days
+        }
+        return $FullTable
+               
+}
+
