fcd6da9799bf6d62a081e1fa46f6cc480871339f
devinziegler <devin.ziegler@mymail.champlain.edu>
2024-10-10 11:57:00 -0400

    update

--- Week 5/web_request_scripts.ps1
+++ Week 5/web_request_scripts.ps1
@@ -9,7 +9,13 @@ $scraped_page.links
 #Display Only URL and its text
 $scraped_page.Links.Href
 
-# Get Outer tect of every element with the tag h2
-$h2s=$scraped_page.ParsedHtml.body.GetElementsByTagName("h2") | where { $_.getAttributeNode("class").Value -ilike "div-3"}
+# Get Outer text of every element with the tag h2
+$h2s=$scraped_page.ParsedHtml.body.GetElementsByTagName("h2") | select outerText
 
 $h2s
+
+# Print innerText of every div element that has the class as "div-1"
+$divs1=$scraped_page.ParsedHtml.body.getElementsByTagName("div") | where { `
+$_.getAttributeNode("class").Value -ilike "div-1"} | select innerText
+
+$divs1
