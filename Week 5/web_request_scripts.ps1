1f92f7dc4731b50417775b53f34f53936c7cb157
devinziegler <devin.ziegler@mymail.champlain.edu>
2024-10-10 11:55:25 -0400

    added we request scripts

---
+++ Week 5/web_request_scripts.ps1
@@ -0,0 +1,15 @@
+﻿$scraped_page = Invoke-WebRequest -URI http://10.0.17.10/ToBeScraped.html
+
+# Get a count of the links in the page
+$scraped_page.links.Count
+
+# Display links as HTML Element
+$scraped_page.links
+
+#Display Only URL and its text
+$scraped_page.Links.Href
+
+# Get Outer tect of every element with the tag h2
+$h2s=$scraped_page.ParsedHtml.body.GetElementsByTagName("h2") | where { $_.getAttributeNode("class").Value -ilike "div-3"}
+
+$h2s
