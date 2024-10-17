e6591e20b9cf4ae035465f79478e7ae3c3966e75
devinziegler <devin.ziegler@mymail.champlain.edu>
2024-10-17 13:49:04 -0400

    Completed First Step In Midterm

---
+++ Midterm/logic.ps1
@@ -0,0 +1,18 @@
+﻿function getIoc() {
+    $page = Invoke-WebRequest -TimeoutSec 2 http://10.0.17.5/IOC.html
+
+    $trs = $page.ParsedHtml.body.GetElementsByTagName("tr")
+
+    $FullTable = @()
+    for($i=1; $i -lt $trs.length; $i++) {
+
+        $tds = $trs[$i].getElementsByTagName("td")
+
+        $FullTable += [PSCustomObject]@{
+            "Pattern" = $tds[0].innerText; `
+            "Description" = $tds[1].innerText; `
+
+        }
+    }
+    return $FullTable 
+}
---
+++ Midterm/main.ps1
@@ -0,0 +1,5 @@
+﻿. (Join-Path $PSScriptRoot logic.ps1)
+
+$fullTable = getIoc
+
+$fullTable | Select-Object "Pattern", "Description" | Format-Table
