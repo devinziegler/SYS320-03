8239d9f3b313e7276f759e3394eaf074a7e1178c
devinziegler <devin.ziegler@mymail.champlain.edu>
2024-11-03 19:19:16 -0500

    done

--- week 9/ActiveIPList.bash
+++ week 9/ActiveIPList.bash
@@ -2,19 +2,20 @@
 
 # Usage: bash ActiveIPList.bash 10.0.17
 
-[ $# -ne 1] && "Usage: $0 <Prefix>" && exit 1
+[ $# -ne 1 ] && "Usage: $0 <Prefix>" && exit 1
 
 # Prefix is the first input
 
 prefix=$1
 
 # Vefify input lenth
- 15 [ ${#prefix} -lt  5  ] && \
- 16 printf "Prefix length is too short\nPrefix example: 10.0.17\n" && \
- 17 exit 1
- 18 
- 19 for i in {1..254}
- 20 do 
- 21         ping -c 1 "$prefix.$i" | grep "success"
- 22 done
+[ ${#prefix} -lt  5  ] && \
+printf "Prefix length is too short\nPrefix example: 10.0.17\n" && \
+exit 1
+
+for i in {1..254}
+do 
+         ping -c 1 "$prefix.$i" | grep "bytes from" | \
+	 grep -o -E "[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}"
+done
 
